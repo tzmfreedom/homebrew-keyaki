fd6d64bb4d10baf842a7e91a5d6408bf2a1b804f
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2017-03-20 23:47:12 +0900

    Update sha-1 hash

--- keyaki.rb
+++ keyaki.rb
@@ -8,11 +8,11 @@ class Keyaki < Formula
   if Hardware::CPU.is_64_bit?
     @@binname = 'keyaki-darwin-amd64'
     url "https://github.com/tzmfreedom/keyaki/releases/download/v#{@@version}/#{@@binname}"
-    sha256 'c0890a340e4bf4720d8b505656ba4a993aebb2dc4db439f974e3adc3808846d1'
+    sha256 'c43012aed234092d020fffa0ca3160bef08dd809bf006a6e7eb05ac84dccf50a'
   else
     @@binname = 'keyaki-darwin-386'
     url "https://github.com/tzmfreedom/keyaki/releases/download/v#{@@version}/#{@@binname}"
-    sha256 '24d823010cd5a702b93dd8c921a4af913b6db9ce2bc764ba48c8fcb3cf18ebd5'
+    sha256 'cd6c27fc7e2aced68ce79ffeaac1a13c7cd42db3ceebf8e351ef2296b84f5c1b'
   end
 
   def install
